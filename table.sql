2488d2a75c6d97a120de2e88676bd37afaddd1fe
David Dehghan <717686+ddehghan@users.noreply.github.com>
2019-10-08 16:08:58 -0700

    Update table.sql
    added index

--- table.sql
+++ table.sql
@@ -5,3 +5,5 @@ CREATE TABLE "session" (
 )
 WITH (OIDS=FALSE);
 ALTER TABLE "session" ADD CONSTRAINT "session_pkey" PRIMARY KEY ("sid") NOT DEFERRABLE INITIALLY IMMEDIATE;
+
+CREATE INDEX "IDX_session_expire" ON "session" ("expire") 
