DefinitelyTyped/DefinitelyTyped

commit d7bd70c33f6102acb5d9c3e04c477d17df4af72a

🤖 Merge PR #67347 `superagent`: refine type for `Response#headers` by @OliverJAsh
* `superagent`: refine type for `Response#headers`

* Update `header`

types/superagent/index.d.ts
@@ -96,8 +96,8 @@ declare namespace request {
         forbidden: boolean;
         get(header: string): string;
         get(header: "Set-Cookie"): string[];
-        header: any;
-        headers: any;
+        header: { [index: string]: string };
+        headers: { [index: string]: string };
         info: boolean;
         links: Record<string, string>;
         noContent: boolean;

voxpelli/node-connect-pg-simple

commit 649888c887b418bb67d9ce638cc05435cc1ede74

test: fix @types/superagent type regression
Regression caused by https://github.com/DefinitelyTyped/DefinitelyTyped/pull/67347 and its interaction with express-session I believe.

The value is a string[], not a string

test/integration/express.spec.js
@@ -86,7 +86,13 @@ describe('Express', () => {
       return request(app)
         .get('/')
         .then(res => {
-          const sessionCookie = new Cookie(res.header['set-cookie'][0]);
+          const cookieHeader = /** @type {string[]|undefined} */ (res.header['set-cookie']);
+
+          if (!cookieHeader || !cookieHeader[0]) {
+            throw new TypeError('Unexpected cookie header');
+          }
+
+          const sessionCookie = new Cookie(cookieHeader[0]);
           const cookieValue = decodeURIComponent(sessionCookie.value);
 
           cookieValue.slice(0, 2).should.equal('s:');